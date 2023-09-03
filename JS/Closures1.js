e9e63190508f047f03959dc678f98d6a2e827d74
ktatoglan <k.tatoglan@gmail.com>
2023-09-04 00:40:55 +0300

    Create Closures1.js

---
+++ JS/Closures1.js
@@ -0,0 +1,12 @@
+function printName() {
+  const helloName = "Hello John";
+
+  function inner() {
+    console.log(helloName);
+  }
+
+  return inner;
+}
+
+const sayHello = printName();
+sayHello();
